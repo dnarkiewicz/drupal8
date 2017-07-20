4a6f2306b0fd5d33db2f9e9be067a5d6175193ee
Dan Narkiewicz <dnarkiewicz@ctacorp.com>
2017-07-20 16:09:43 -0400

    correct env.php putenv format

--- docker/drupal/rootfs/etc/php/env.php
+++ docker/drupal/rootfs/etc/php/env.php
@@ -1,11 +1,11 @@
 <?php
 
-putenv('D8_DB_DATABASE',  'd8');
-putenv('D8_DB_USERNAME',  'root');
-putenv('D8_DB_PASSWORD',  '');
-putenv('D8_DB_PREFIX',    '');
-putenv('D8_DB_HOST',      'db');
-putenv('D8_DB_PORT',      '3306');
-putenv('D8_DB_NAMESPACE', 'Drupal\Core\Database\Driver\mysql');
-putenv('D8_DB_DRIVER',    'mysql');
-putenv('D8_CONFIG_SYNC',  'rvJNE4qNpszJvg0PSepUw3PYR9TO87esGw6suCxFpbX49OEGKUfc2hVcb33NvFWiSZXLLdTQHw');
+putenv('D8_DB_DATABASE=d8');
+putenv('D8_DB_USERNAME=root');
+putenv('D8_DB_PASSWORD=');
+putenv('D8_DB_PREFIX=');
+putenv('D8_DB_HOST=db');
+putenv('D8_DB_PORT=3306');
+putenv('D8_DB_NAMESPACE=Drupal\Core\Database\Driver\mysql');
+putenv('D8_DB_DRIVER=mysql');
+putenv('D8_CONFIG_SYNC=rvJNE4qNpszJvg0PSepUw3PYR9TO87esGw6suCxFpbX49OEGKUfc2hVcb33NvFWiSZXLLdTQHw');
